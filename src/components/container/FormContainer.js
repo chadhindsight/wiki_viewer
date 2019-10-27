1696843a02812832631d02fc644ea2e616beea63
Chad Hinds <chadrickj8@gmail.com>
2019-10-27 18:38:27 -0400

    Wrap Form container around input field and output it

--- src/components/container/FormContainer.js
+++ src/components/container/FormContainer.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Input from '../presentational/Input';
+import Header from '../presentational/Header';
+import Input from '../presentational/Input.jsx';
 
-class formContainer extends Component {
+class FormContainer extends Component {
     constructor() {
         super()
         this.state = {
             title: ""
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value})
+    }
+
+    render() {
+        const { title } = this.state;
+
+        return (
+            <form id="article-form">
+                <Input
+                    text="Search"
+                    label="seo_title"
+                    type="text"
+                    id="seo_title"
+                    value={title}
+                    handleChange={this.handleChange}
+                />
+            </form>
+        )
+    }
 }
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
+
+export default FormContainer;
