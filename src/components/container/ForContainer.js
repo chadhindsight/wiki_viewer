9602bf6a3509bdd5c1db39b343b63cf851fa7712
Chad Hinds <chadrickj8@gmail.com>
2019-10-27 11:03:17 -0400

    Initial setup commit

---
+++ src/components/container/ForContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class formContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            title: ""
+        }
+    }
+
+}
