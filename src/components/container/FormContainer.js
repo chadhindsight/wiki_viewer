a17c1d770f18eb878f15145ab64255923cf44d53
Chad Hinds <chadrickj8@gmail.com>
2019-10-27 15:06:05 -0400

    Import Input component

---
+++ src/components/container/FormContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Input from '../presentational/Input';
+
+class formContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            title: ""
+        }
+    }
+
+}
