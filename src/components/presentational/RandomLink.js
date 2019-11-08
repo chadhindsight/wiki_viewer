6de80e67d40dd1d17cbff2f03670f67ee7a5c3e3
Chad Hinds <chadrickj8@gmail.com>
2019-11-08 14:21:03 -0500

    Move link tag code into a seperate presentaional component

---
+++ src/components/presentational/RandomLink.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RandomLink = (props) => (
+    <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" rel="noopener">
+        <img src="https://image.ibb.co/fR5OX5/random.png" alt="Shuffle Icon" /></a> 
+)
+
+export default RandomLink;
