e6a0f92947412d69e34481ff0f0bf5e21ed18e9d
Chad Hinds <chadrickj8@gmail.com>
2020-01-04 21:35:03 -0500

    change alt text for the image for shulle icon

--- src/components/presentational/RandomLink.js
+++ src/components/presentational/RandomLink.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+// Store the href for the random wiki entry
 const RandomLink = (props) => (
     <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" rel="noopener">
-        <img src="https://image.ibb.co/fR5OX5/random.png" alt="Shuffle Icon" /></a> 
+        <img src="https://image.ibb.co/fR5OX5/random.png" alt="shuffle icon" /></a> 
 )
 
 export default RandomLink;
