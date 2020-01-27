9c15fbbad4cdbf94da04ee49b04b1b7bc1a52e39
Chad Hinds <chadrickj8@gmail.com>
2020-01-27 16:43:02 -0500

    change h1 text

--- src/components/presentational/Header.js
+++ src/components/presentational/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () =>(
     <div id="">
-        <h1>WIKI SEARCH</h1>
+        <h1>WIKI SEARCH!</h1>
         <img src="https://image.ibb.co/e6vOFQ/wikipedia.png" alt="Wikipedia Logo"/>
     </div>
 )
